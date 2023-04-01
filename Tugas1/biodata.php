339bc361c72bf3372679d35c4ec68f794f1e866a
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2023-04-01 22:57:16 +0700

    Update biodata.php

--- Tugas1/biodata.php
+++ Tugas1/biodata.php
@@ -1,1 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Diri Gilang Ramadhan</title>
+</head>
+<body style="background-color: #FAFAD2">
 
+	<center><h2>Biodata Diri Mahasiswa</h2>
+	<img src="FOTO.jpg" height="190px" width="165px">
+	</center>
+	<br><br><br><br><br>
+
+	<table border="0" style="margin-left: 100px">  
+  		<tr>
+    		<td><b>Nama</b></td>
+    		<td>: Gilang Ramadhan</td>
+  		</tr>
+  		<tr>
+    		<td><b>NPM</b></td>
+    		<td>: 2117051082</td>
+    	</tr>
+    	<tr>
+    		<td><b>Alamat</b></td>
+    		<td>: Gedong Meneng, Kec. Rajabasa, Bandar Lampung</td>
+  		</tr>
+  		<tr>
+    		<td><b>Jenis Kelamin &emsp; &emsp; &emsp; &emsp;</b></td>
+    		<td>: Laki-laki</td>
+    	</tr>
+    	<tr>
+    		<td><b>Tinggi Badan</b></td>
+    		<td>: 168 cm</td>
+  		</tr>
+  		<tr>
+    		<td><b>Berat Badan</b></td>
+    		<td>: 48 kg</td>
+    	</tr>
+    	<tr>
+    		<td><b>Nomor Sepatu</b></td>
+    		<td>: 41</td>
+  		</tr>
+  		<tr>
+    		<td><b>Nomor WA</b></td>
+    		<td>: 082374489177</td>
+    	</tr>
+
+</body>
+</html>
