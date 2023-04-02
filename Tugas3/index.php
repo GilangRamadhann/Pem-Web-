0fe19eb10ed1985bac740c106bf9d06f16e47eb4
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2023-04-02 12:30:54 +0700

    Add files via upload

---
+++ Tugas3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<head></head>
+<body style="background-color: #FAFAD2">
+<Body>
+	
+    <form action="login.php" method="post">
+        <label>Username : </label>
+        <input type="text" name="user"><br><br>
+        <label>Password : </label>
+        <input type="password" name="pass"><br>
+        <input type="submit" value="Login">
+    </form>
+</Body>
+
+</html>
